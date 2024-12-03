e89e44194e4b0ef7f18605f0a3c7d06fd3bbef55
AleksanderWyrwa <152646813+AleksanderWyrwa@users.noreply.github.com>
2024-12-03 13:47:50 +0100

    Update Program.cs

--- Day3/bolcio/AdventOfCodeDay3/AdventOfCodeDay3/Program.cs
+++ Day3/bolcio/AdventOfCodeDay3/AdventOfCodeDay3/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+
+using System;
 using System.IO;
 using System.Text.RegularExpressions;
 
@@ -8,26 +9,33 @@ class Program
     {
         try
         {
+            int sum = 0;
             string line;
             int safeCount = 0;
-            string returningString = ""; 
+            string returningString = "";
             // Open the input file
             using (StreamReader sr = new StreamReader("../../../../../adventofcode3.txt"))
             {
                 while ((line = sr.ReadLine()) != null)
                 {
-                   returningString = FindString(line);
-                   returningString = returningString.Replace(" ", "").Replace("\t", "").Replace("\n", "").Replace("\r", "");
+                    returningString = FindString(line);
+                    returningString = returningString.Replace(" ", "").Replace("\t", "").Replace("\n", "").Replace("\r", "");
                 }
             }
             // Output the result
             Console.WriteLine($"Total String: " + returningString);
             string[] multiplies = returningString.Trim().Split('|');
+
             foreach (string multiplier in multiplies)
-            {
-/*                multiplier.Remove(0, 3).Remove(multiplier.Length - 1);*/
-                Console.WriteLine(multiplier);
+            {   if (multiplier.Length > 0)
+                {
+                    String[] Numbers = FindNumbers(multiplier).Split(',');
+                    sum += Int32.Parse(Numbers[0]) * Int32.Parse(Numbers[1]);
+                    Console.WriteLine(Int32.Parse(Numbers[0]) * Int32.Parse(Numbers[1]) * 2);
+                    Console.WriteLine(sum);
+                }
             }
+            Console.WriteLine(sum);
         }
         catch (Exception e)
         {
@@ -45,7 +53,7 @@ class Program
         string pattern = @"^mul\(\d{1,3},\d{1,3}\)$";
         var indexStart = str.IndexOf("mul(");
         Console.WriteLine(indexStart);
-        var indexEnd = 0; 
+        var indexEnd = 0;
 
         if (indexStart != -1)
         {
@@ -72,9 +80,9 @@ class Program
                 newString = newString + str[i];
             }
         }
-/*        Console.WriteLine(newString);*/
-
+        /*        Console.WriteLine(newString);*/
         var match = Regex.Match(foundString, pattern);
+
         if (match.Success)
         {
             Console.WriteLine("Match found: " + match);
@@ -84,10 +92,27 @@ class Program
             Console.WriteLine("No match found");
         }
 
-        if (indexStart != -1) 
+        if (indexStart != -1)
         {
-            returnString = match.ToString() + "|" + FindString(newString);        
+            returnString = match.ToString() + "|" + FindString(newString);
         };
         return returnString;
+    }
+    static string FindNumbers(string str)
+    {
+        string pattern = @"\d{1,3},\d{1,3}";
+
+        var match = Regex.Match(str, pattern);
+        if (match.Success)
+        {
+            Console.WriteLine("Match found: " + match);
+        }
+        else
+        {
+            Console.WriteLine("No match found");
+        }
+        String matching = match.ToString();
+
+        return matching;
     }
 }
