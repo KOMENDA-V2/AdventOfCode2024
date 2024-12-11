b48d8cac4a0e210aae4b41aa28610aff6cffd81c
AleksanderWyrwa <152646813+AleksanderWyrwa@users.noreply.github.com>
2024-12-11 23:20:03 +0100

    Good for part one and brute forcing part 2

---
+++ day11/bolcio/AdventOfCode11/AdventOfCode11/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Numerics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Enter the number of iterations:");
+            int numIterations;
+            if (!int.TryParse(Console.ReadLine(), out numIterations) || numIterations <= 0)
+            {
+                Console.WriteLine("Please enter a valid positive integer.");
+                return;
+            }
+
+            List<BigInteger> updatedRocksList = new List<BigInteger>();
+
+            using (StreamReader sr = new StreamReader("../../../../../adventofcode11.txt"))
+            {
+                string line = sr.ReadLine();
+                if (line != null)
+                {
+                    foreach (string rock in line.Split(' '))
+                    {
+                        updatedRocksList.Add(BigInteger.Parse(rock));
+                    }
+                }
+
+                Dictionary<BigInteger, List<BigInteger>> cache = new Dictionary<BigInteger, List<BigInteger>>();
+
+                for (BigInteger i = 0; i < numIterations; i++)
+                {
+                    List<BigInteger> newRocksList = new List<BigInteger>();
+
+                    foreach (BigInteger rock in updatedRocksList)
+                    {
+                        List<BigInteger> transformedRocks;
+
+                        if (cache.ContainsKey(rock))
+                        {
+                            transformedRocks = cache[rock];
+                        }
+                        else
+                        {
+                            if (rock == 0)
+                            {
+                                transformedRocks = new List<BigInteger> { 1 };
+                            }
+                            else if (rock % 2 == 0) 
+                            {
+                                BigInteger firstHalf = rock / 2;
+                                BigInteger secondHalf = rock - firstHalf;
+                                transformedRocks = new List<BigInteger> { firstHalf, secondHalf };
+                            }
+                            else
+                            {
+                                transformedRocks = new List<BigInteger> { rock * 2024 };
+                            }
+
+                            cache[rock] = transformedRocks;
+                        }
+
+                        newRocksList.AddRange(transformedRocks);
+                    }
+
+                    updatedRocksList = newRocksList;
+                    Console.WriteLine($"Number of rocks after {i + 1} iterations is {updatedRocksList.Count}");
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Exception: {e.Message}");
+        }
+        finally
+        {
+            Console.WriteLine("Execution complete.");
+        }
+    }
+}
